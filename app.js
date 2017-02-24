6ff7fe945e761fddbdb47e50fffd592445dfd5ed
BuildTools <unconfigured@null.spigotmc.org>
2017-02-24 19:52:25 +0900

    edit app.js

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.use(express.static('./public'));
 //public 내부에 있던 모든 폴더를 그냥 접속하면 보여주는 방식으로 만들겠다.
 const server = http.createServer(app);
 const env = {
+    NODE_IP: process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    NODE_PORT: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+}
+
+/*const env = {
     NODE_IP: '172.30.35.182', // openshift node.js 서버 아이피. 역시 걍 이건가봐
     NODE_PORT: '8080'
-};
+};*/
 
 console.log('Listening...');
 server.listen(env.NODE_PORT, env.NODE_IP, (error) => {
