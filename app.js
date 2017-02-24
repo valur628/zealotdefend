1e5594d07cca37af4b58f3cdce3494fe443eb82e
BuildTools <unconfigured@null.spigotmc.org>
2017-02-24 15:52:34 +0900

    remote address update

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const http = require('http');
 app.use(express.static('./public'));
 //public 내부에 있던 모든 폴더를 그냥 접속하면 보여주는 방식으로 만들겠다.
 const server = http.createServer(app);
+const env = {
+    NODE_IP: '172.30.35.182',
+    NODE_PORT: 8080
+};
 
 console.log('Listening...');
-server.listen(8080, 'localhost', (error) => {
+server.listen(env.NODE_PORT, env.NODE_IP, (error) => {
 if (error) {
     throw new Error('ERROR! CAN NOT BINDING PORT!');
     return;
